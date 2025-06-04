4e0541923f43209da9c76d11d4ff2e917f3251fd
Kalinduwije22 <kala22293@gmail.com>
2025-06-04 21:30:39 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,35 @@ import streamlit as st
 import os
 import urllib.request
 
-MODEL_URL = "https://drive.google.com/uc?export=download&id=1TKWI5I7D32YOFi3oQpQlV5kPz9jBRJ8K"
+def download_file_from_google_drive(file_id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+
+
+FILE_ID = "1TKWI5I7D32YOFi3oQpQlV5kPz9jBRJ8K"
 MODEL_PATH = "monthly_income_model.pkl"
 
 # Download if not present
